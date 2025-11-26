9d7d2a4db7bede8ac3948e006c8b9292b10194bd
Esmanur Yılmaz <esmanuryilmaz0627@gmail.com>
2025-11-26 23:10:46 +0300

    Create VucutKitleEndeksi.java

---
+++ VucutKitleEndeksi.java
@@ -0,0 +1,8 @@
+public class VucutKitleEndeksi {
+    public static void main(String[] args) {
+        int boy= 163;
+        double kilo= 46.3;
+        double BMI = kilo/boy*boy;
+        System.out.println("BMI="+BMI);
+    }
+}
